9de2cf8e70d9ed6775cefcdc9380dd927f9ffc8e
docwei <doc_wei@163.com>
2018-10-28 18:59:44 +0800

    更换code

--- imageupload_lib/src/main/java/com/docwei/imageupload_lib/constant/ImageConstant.java
+++ imageupload_lib/src/main/java/com/docwei/imageupload_lib/constant/ImageConstant.java
@@ -1,7 +1,7 @@
 package com.docwei.imageupload_lib.constant;
 
 public class ImageConstant {
-    public static final int REQUEST_CODE_IAMGES = 1;// 拍照或者选择相册使用的请求码
+    public static final int REQUEST_CODE_IAMGES = 999;// 拍照或者选择相册使用的请求码
     public static final String SELECTED_IAMGES = "images";// 获取选择的图片的key
     public static final String TYPE = "type";  // 选择图片的类型
     public static final String COUNT = "count"; // 需要选择的数量
